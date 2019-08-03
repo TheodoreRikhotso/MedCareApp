6a6aea57a841ea280285000a65b6fa3d461a323a
TheodoreRikhotso <rikhotsothoko@gmail.com>
2019-08-03 13:00:46 +0200

    updated

--- app/src/main/java/com/example/user/medcareapp/AppointmentFragment.java
+++ app/src/main/java/com/example/user/medcareapp/AppointmentFragment.java
@@ -9,14 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AppointmentFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AppointmentFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AppointmentFragment extends Fragment {
 
 
--- app/src/main/java/com/example/user/medcareapp/HomeActivity.java
+++ app/src/main/java/com/example/user/medcareapp/HomeActivity.java
@@ -3,31 +3,71 @@ package com.example.user.medcareapp;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            fragmentManager = getSupportFragmentManager();
+            fragment =new HomeFragment();
+
             switch (item.getItemId()) {
+
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+
+                    fragment = new HomeFragment();
+
+
+                    break;
+//
+                case R.id.navigation_tips:
+
+                    fragment = new TipsFragment();
+
+
+                    break;
+
+                case R.id.navigation_emergency:
+
+                    fragment = new EmergencyFragment();
+
+
+                    break;
+
+                case R.id.navigation_appointments:
+
+                    fragment = new AppointmentFragment();
+
+
+                    break;
+
+                case R.id.navigation_progress:
+
+                    fragment = new ProgressFragment();
+
+
+                    break;
+
+
+
+
             }
-            return false;
+            final FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.content, fragment).commit();
+            return true;
         }
     };
 
@@ -36,9 +76,17 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        HomeFragment f = new HomeFragment();
+        FragmentManager manager=getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+
+
+
+        transaction.add(R.id.content,f);
+        transaction.commit();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+        navigation.setSelectedItemId(R.id.navigation_home);
+    }
 }
